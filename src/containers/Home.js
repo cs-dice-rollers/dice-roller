5fc421fa44e196e34224095509588335a1bec686
Michael Young <michaeljy@gmail.com>
2017-01-17 14:07:10 -0600

    Fetch dice.

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -46,8 +46,37 @@ export default class Home extends Component {
     navigate: PropTypes.func.isRequired
   };
 
-  rollDice() {
-    // do something
+  componentWillMount() {
+    this.rollDice();
+  }
+
+  rollDice = () => {
+    // Move this into an action thingamajig
+
+    const payloadBody = {
+      method: 'generateIntegers',
+      jsonrpc: '2.0',
+      params: {
+        apiKey: '5047c35e-b0ac-4666-8619-1fad7327e727',
+        n: 2,
+        min: 1,
+        max: 20
+      },
+      id: 1,
+    };
+
+    fetch('https://api.random.org/json-rpc/1/invoke', {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(payloadBody)
+    })
+    .then(response => response.json())
+    .then((response) => {
+      console.log('RESPONSE', response.result.random.data);
+    });
   }
 
   toHistory = () => {
